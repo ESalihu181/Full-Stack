a9a966f9756956d92e0c51a1396bcd1be8c93ddf
Ervin Salihu <es51553@ubt-uni.net>
2023-08-23 18:21:58 +0200

    Complex state

--- Part1/lecture/src/App.jsx
+++ Part1/lecture/src/App.jsx
@@ -1,26 +1,29 @@
 const Hello = (props) => {
-  console.log(props)
+  const { name, age } = props
+  const bornYear = () => new Date().getFullYear() - age
+  
+
   return (
     <div>
       <p>
-        Hello {props.name}, you are {props.age} years old
+        Hello {name}, you are {age} years old
       </p>
+      <p>So you were probably born in {bornYear()}</p>
     </div>
   )
 }
 
 const App = () => {
-  const friends = [
-    {name: 'Peter', age: 4},
-    {name: 'Maya', age: 10},
-  ]
+  const name = 'Peter'
+  const age = 10
 
-  return(
+  return (
     <div>
-      <p>{friends[0].name} {friends[0].age}</p>
-      <p>{friends[1].name} {friends[1].age}</p>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
     </div>
   )
-  
 }
+
 export default App
